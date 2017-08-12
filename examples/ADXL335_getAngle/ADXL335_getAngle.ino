3743968e4baedb48ff4fc798e694ae70259cff95
lin <vsnetlin@163.com>
2017-08-12 21:33:06 +0800

    Add Example for function getAngle.update keywords.

---
+++ examples/ADXL335_getAngle/ADXL335_getAngle.ino
@@ -0,0 +1,27 @@
+/*
+ * ADXL335_example.ino - Example for ADXL335 library
+ * Written by MrTijn/Tijndagamer
+ * Copyright (c) 2016 MrTijn/Tijndagamer
+ * Released under the MIT license
+ */
+
+#include <ADXL335.h>
+
+ADXL335 adxl335(A0, A1, A2, 3.3);
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    Serial.print(adxl335.getAngle(X));
+    Serial.print("\t");
+    Serial.print(adxl335.getAngle(Y));
+    Serial.print("\t");
+    Serial.print(adxl335.getAngle(Z));
+    Serial.print("\t");
+    Serial.println();
+    delay(500); 
+}
